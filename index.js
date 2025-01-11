a93a4d0d493d301da0c8991d2109f07d1ba036f2
Ishmeet Kaur <ishmeetkaur364@gmail.com>
2025-01-11 17:47:28 +0000

    added

--- index.js
+++ index.js
@@ -9,9 +9,15 @@ app.use(express.json());
 app.get("/",(req,res)=>{
     res.status(200).json({
         message: "Server is up and running",
-    })
+    });
+});
+
+app.get("*" , (req,res) =>{
+    res.status(404).json({
+        message: "Path does not exists",
+    });
 });
 
 app.listen(PORT,()=>{
-    console.log(`Server is running on port ${PORT}`)
-});
+    console.log(`Server is running on port ${PORT}`);
+})
